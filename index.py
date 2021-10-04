d6f68177af27a80cc3e3584408dfacba6bc50304
MateCon <68882595+MateCon@users.noreply.github.com>
2021-10-04 16:16:41 -0300

    optimize fibonacci with memoization

--- index.py
+++ index.py
@@ -2,8 +2,10 @@ def factorial(n):
     if n <= 1: return 1
     return n * factorial(n - 1)
 
-def fibonacci(n):
+def fibonacci(n, memo=dict()):
     if n <= 0: return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    if n in memo: return memo[n]
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return memo[n]
 
-print(fibonacci(5))
+print(fibonacci(100))
