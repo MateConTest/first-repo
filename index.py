0b827a940737127593f5c1113647e88ffaa5a689
MateCon <68882595+MateCon@users.noreply.github.com>
2021-10-04 16:10:57 -0300

    implement fibonacci

--- index.py
+++ index.py
@@ -2,5 +2,8 @@ def factorial(n):
     if n <= 1: return 1
     return n * factorial(n - 1)
 
-for i in range(1, 11):
-    print(factorial(i))
+def fibonacci(n):
+    if n <= 0: return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(5))
